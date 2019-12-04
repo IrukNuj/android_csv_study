11b260672ab93343abac736c23df66bd208f72ad
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2019-12-04 19:33:28 +0900

    :+1: add data clas

---
+++ app/src/main/java/com/example/csv_study/Shelter.kt
@@ -0,0 +1,11 @@
+package com.example.csv_study
+
+data class Shelter (
+    //名称,概略
+    var id: Int? = null,
+    var name: String? = null,
+    var weight: Float? = null,
+    var types: String? = null,
+    var statuses: String? = null,
+    var abilities: String? = null
+){}
